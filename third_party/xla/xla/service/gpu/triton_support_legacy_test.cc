81220633e4f6edfb2eb2e642470151669e6d25cd
Benjamin Chetioui <bchetioui@google.com>
2024-07-16 06:59:08 -0700

    [XLA:GPU] Make the generic Triton emitter preserve tensor ranks during codegen.
    Triton seems to have become robust enough to allow us to do this (at least for
    non-GEMMs).
    
    Notably, this requires the introduction of an explicit lowering path for
    `reshape`s and `bitcast`s. Of course, this change required updating a lot of
    change detectors `Filecheck` tests.
    
    I took the opportunity to delete some of these that no longer brought useful
    coverage after the switch to the new generic Triton emitter. I made the
    remaining tests more robust by focusing the matchers on key properties of the
    generated code, and also by executing them (without running the optimization
    pipeline) if it seemed warranted.
    
    PiperOrigin-RevId: 652824820

--- third_party/xla/xla/service/gpu/triton_support_legacy_test.cc
+++ third_party/xla/xla/service/gpu/triton_support_legacy_test.cc
@@ -315,7 +315,8 @@ ENTRY e {
                     mlir_context_),
       tsl::testing::StatusIs(
           absl::StatusCode::kInternal,
-          ::testing::HasSubstr("Failed to create Triton module for fusion")));
+          ::testing::HasSubstr(
+              "Failed to verify Triton module before optimizations")));
 }
 
 TEST_F(TritonSupportTestBase,
